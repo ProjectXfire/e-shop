356c8a402670a26adff6354ea3530f6bc10aab55
gvbdeveloper <gabriel.vargas.bravo@gmail.com>
2025-03-27 15:07:59 -0500

    add: cache service product

--- src/app/(shop)/(routes)/products/[slug]/page.tsx
+++ src/app/(shop)/(routes)/products/[slug]/page.tsx
@@ -1,19 +1,36 @@
+import type { Metadata } from "next";
 import { redirect } from "next/navigation";
-import { getProductBySlug } from "@/core/shop/services/get-product.service";
+import { getProductBySlugCache } from "@/core/shop/services/get-product.service";
 import ProductContainer from "@/app/(shop)/_components/product-container/ProductContainer";
 import ProductFeatures from "@/app/(shop)/_components/product-features/ProductFeatures";
 import SlideShow from "@/app/(shop)/_components/slide-show/SlideShow";
 import MaxWidthContainer from "@/shared/components/containers/max-width-container/MaxWidthContainer";
 import FadeinContainer from "@/shared/components/containers/fadein-container/FadeinContainer";
+import TitleAnimated from "@/shared/components/animations/title-animated/TitleAnimated";
+import ProductStock from "@/app/(shop)/_components/product-stock/ProductStock";
 
 interface Props {
   params: Promise<{ slug: string }>;
 }
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+  const product = await getProductBySlugCache(slug);
+  return {
+    title: product?.title,
+    description: product?.description,
+    openGraph: {
+      title: product?.title,
+      description: product?.description,
+      images: product?.images,
+    },
+  };
+}
+
 async function ProductPage({ params }: Props): Promise<React.ReactElement> {
   const { slug } = await params;
 
-  const product = await getProductBySlug(slug);
+  const product = await getProductBySlugCache(slug);
 
   if (!product) redirect("/");
 
@@ -22,7 +39,11 @@ async function ProductPage({ params }: Props): Promise<React.ReactElement> {
       <FadeinContainer>
         <ProductContainer>
           <SlideShow images={product.images} />
-          <ProductFeatures product={product} />
+          <section className="flex flex-col gap-[30px] px-[15px] py-[10px] pb-[30px]">
+            <TitleAnimated title={product.title} subtitle={`Price: $${product.price.toString()}`} />
+            <ProductStock slug={slug} />
+            <ProductFeatures product={product} />
+          </section>
         </ProductContainer>
       </FadeinContainer>
     </MaxWidthContainer>
--- src/core/shop/services/get-product.service.ts
+++ src/core/shop/services/get-product.service.ts
@@ -1,4 +1,7 @@
+"use server";
+
 import type { Product } from "../models/product";
+import { unstable_cache } from "next/cache";
 import { prisma } from "@/shared/config/prisma";
 import { productMapper } from "../mappers/product.mapper";
 
@@ -13,5 +16,39 @@ export async function getProductBySlug(slug: string): Promise<Product | null> {
     return product;
   } catch (error) {
     return null;
+  }
+}
+
+export async function getProductBySlugCache(slug: string): Promise<Product | null> {
+  try {
+    const returnCache = unstable_cache(
+      async () => {
+        return await prisma.product.findUnique({
+          where: { slug },
+          include: { images: { select: { url: true } } },
+        });
+      },
+      [`product-${slug}`],
+      { revalidate: 3600, tags: ["products"] }
+    );
+    const data = await returnCache();
+    if (!data) throw new Error("Product not found");
+    const product = productMapper(data);
+    return product;
+  } catch (error) {
+    return null;
+  }
+}
+
+export async function getProductStockBySlug(slug: string): Promise<number> {
+  try {
+    const data = await prisma.product.findUnique({
+      where: { slug },
+      select: { inStock: true },
+    });
+    if (!data) throw new Error("Product not found");
+    return data.inStock;
+  } catch (error) {
+    return 0;
   }
 }
